933d85e0d9f45228630c0d99706db635b7699eef
azranabbasi3 <azranabbasi708@gmail.com>
2025-03-26 21:54:04 +0500

    1st day practice

---
+++ index.js
@@ -0,0 +1,26 @@
+// const app = require('./app');
+import {add} from './app.js';
+
+// // const x=40;
+// // const y=20;
+// // const z=x+y;
+// // console.log(z);
+
+// console.log(app.x);
+// console.log(app.y);
+// console.log(app.z());
+
+
+// const a = [1,6,8,2,4,7]
+// const b = a.filter((num)=>{
+//     return num>2
+// })
+// console.log(b)
+
+// const a = [1,6,8,2,4,7]
+// const b = a.map((num)=>{
+//     return num*2
+// })
+// console.log(b)
+
+console.log(add());
