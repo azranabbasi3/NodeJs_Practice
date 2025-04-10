b48515b652d0b1f4205bbd925d0f80ac5a23298f
azranabbasi3 <azranabbasi708@gmail.com>
2025-04-10 19:46:06 +0500

    Crud  with mongoose

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const mongoose = require("mongoose");
 
-const main = async () => {
+const connectDB = async () => {
   await mongoose.connect("mongodb://localhost:27017/Infini");
-  const mobileSchema = new mongoose.Schema({
-    name: String,
-  });
+};
 
+connectDB();
+
+const mobileSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  color: String,
+});
+const saveInDB = async () => {
   const mobileModel = mongoose.model("products", mobileSchema);
-  let data = new mobileModel({ name: "vivo y10" });
+  let data = new mobileModel({ name: "vivo y13", price: 4000, color: "green" });
   let reult = await data.save();
   console.log(reult);
 };
-main()
+// saveInDB();
+
+const updateInDB = async () => {
+  const mobileModel = mongoose.model("products", mobileSchema);
+  let data = await mobileModel.updateOne(
+    { name: "vivo y20" },
+    { $set: { price: 3000, name: "vivo y17" } }
+  );
+  console.log(data);
+};
+// updateInDB();
+
+const deleteInDB = async () => {
+  const mobileModel = mongoose.model("products", mobileSchema);
+  let data = await mobileModel.deleteOne({ name: "vivo y17" });
+  console.log(data);
+};
+// deleteInDB();
+
+const findInDB = async () => {
+  const mobileModel = mongoose.model("products", mobileSchema);
+  let data = await mobileModel.find();
+  console.log(data);
+};
+// findInDB();
